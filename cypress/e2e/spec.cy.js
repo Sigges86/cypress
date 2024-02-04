4da8c6c3fb412c8beff3e7d3a7dc8c8fb065e16e
Sigges86 <sigges.86@gmail.com>
2024-02-04 01:18:24 +0100

    updates

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+import { describe } from "node:test";
+
+describe('My first test', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+}));
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
